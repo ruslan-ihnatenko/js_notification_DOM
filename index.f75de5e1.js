ebc50ba7128cbeb110e878dc31a800290a0a1049
Ruslan Ihnatenko <ua.ignatenko@gmail.com>
2025-01-11 02:05:55 +0100

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,19 @@
+"use strict";
+const pushNotification = (posTop, posRight, title, description, type)=>{
+    const body = document.querySelector("body");
+    body.insertAdjacentHTML("afterbegin", `
+    <div class='notification ${type}'>
+      <h2 class='title'>${title}</h2>
+      <p>${description}</p>
+    </div>
+    `);
+    const insertedEl = body.querySelector(`.notification.${type}`);
+    insertedEl.style.top = `${posTop}px`;
+    insertedEl.style.right = `${posRight}px`;
+    setTimeout(()=>insertedEl.style.visibility = "hidden", 2000);
+};
+pushNotification(10, 10, "Title of Success message", "Message example.\n Notification should contain title and description.", "success");
+pushNotification(150, 10, "Title of Error message", "Message example.\n Notification should contain title and description.", "error");
+pushNotification(290, 10, "Title of Warning message", "Message example.\n Notification should contain title and description.", "warning");
+
+//# sourceMappingURL=index.f75de5e1.js.map
